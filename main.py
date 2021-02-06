df93ef75f8c4e0b7b729225e6dee5d67f1714f28
Patrycja <pati.iwanek@gmail.com>
2021-02-06 18:45:36 +0100

    extended quiz with template and added question as a list

--- main.py
+++ main.py
@@ -1,11 +1,30 @@
 from flask import Flask
+from flask import render_template
 
 app = Flask(__name__)
 
+# konfiguracja aplikacji
+app.config.update(dict(SECRET_KEY='bardzosekretnawartosc',
+                       ))
+
+# lista pytań
+DANE = [{
+    'pytanie': 'Stolica Hiszpanii to:',  # pytanie
+    'odpowiedzi': ['Madryt', 'Hiszpania', 'Barcelona'],  # odpowiedzi
+    'odpok': 'Madryt'},  # poprawna odpowiedz
+    {
+    'pytanie': 'Symbol pierwiastka Żelazo, to:',
+    'odpowiedzi': ['Ze', 'Fe', 'St'],
+    'odpok': 'Fe' },
+    {
+    'pytanie': 'Objętość sześcianu o boku 5 wynosi:',
+    'odpowiedzi': ['25', '125', '15'],
+    'odpok': '125cm'},
+]
 
 @app.route('/')
 def index():
-    return 'Cześć, tu Python!'
+    return render_template('index.html', pytania=DANE)
 
 
 if __name__ == '__main__':
