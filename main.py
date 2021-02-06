94e66e9fbd133f053266575939500196598929b5
Patrycja <pati.iwanek@gmail.com>
2021-02-06 19:45:14 +0100

    fully worked simple app

--- main.py
+++ main.py
@@ -16,28 +16,28 @@ DANE = [{
     {
     'pytanie': 'Symbol pierwiastka Żelazo, to:',
     'odpowiedzi': ['Ze', 'Fe', 'St'],
-    'odpok': 'Fe' },
+    'odpok': 'Fe'},
     {
-    'pytanie': 'Objętość sześcianu o boku 5 wynosi:',
-    'odpowiedzi': ['25', '125', '15'],
+    'pytanie': 'Objętość sześcianu o boku 5cm wynosi:',
+    'odpowiedzi': ['25cm', '125cm', '15cm'],
     'odpok': '125cm'},
 ]
 
-@app.route('/', methods =['GET', 'POST'])
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    if request=='POST':
+
+    if request.method == 'POST':
         punkty = 0
-        odpowiedzi = request.form()
+        odpowiedzi = request.form
 
         for pnr, odp in odpowiedzi.items():
             if odp == DANE[int(pnr)]['odpok']:
-                punkty+=1
+                punkty += 1
 
-        print("punkty: " + punkty)
         flash('Liczba poprawnych odpowiedzi, to: {0}'.format(punkty))
         return redirect(url_for('index'))
 
-
+    # return 'Cześć, tu Python!'
     return render_template('index.html', pytania=DANE)
 
 
